c8314f26ee4064cc1161c5830f92f94bc84b1ac9
yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-02 16:31:06 +0530

    Day1 programs done

---
+++ Day1/Assignment.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Assignment
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter any number:");
+int a=s.nextInt();
+int result=++a;
+System.out.println(result);
+}
+}
---
+++ Day1/Relational.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Relational
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter first number:");
+int a=s.nextInt();
+System.out.print("Enter second number:");
+int b=s.nextInt();
+System.out.println(a==b);
+System.out.println(a!=b);
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+}
+}
