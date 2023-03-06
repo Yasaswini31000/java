96728d46b1c23b0f7f5a98bb70a01da3f07cba8b
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-06 23:58:33 +0530

    update

---
+++ Day9/Functions.java
@@ -0,0 +1,11 @@
+public class Functions
+{
+static int sum(int x) 
+{
+return 5 + x;
+}
+public static void main(String[] args) 
+{
+System.out.println(sum(19));
+}
+}
---
+++ Day9/Var.java
@@ -0,0 +1,10 @@
+class Var
+{
+public static void main(String[] args)
+{
+var a=5;
+var b=3;
+var x = a/b;
+System.out.println(x);
+}
+}
