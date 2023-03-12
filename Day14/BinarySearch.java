f8beee0039241b53a3d395473747bf405d37875a
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-12 23:37:40 +0530

    Day14 programs done

--- Day14/BinarySearch.java
+++ Day14/BinarySearch.java
@@ -3,29 +3,30 @@ public class BinarySearch
 public static void main(String args[])
 {
 int arr[]={2,4,6,8,10,12,14,16,18,20,22,24,26,28,30};
-int n=arr.length,x=20,mid;
+int n=arr.length,x=20;
 int low=0,high=arr.length-1;
-for(int i=0;i<n-1;i++)
+int mid=(low+high)/2;
+while(low<=high)
 {
-if(low<=high)
+if(arr[mid]<x)
 {
-mid=low+(high-low)/2;
-if(arr[mid]==x)
+low=mid+1;
+}
+else if(arr[mid]==x)
 {
-System.out.println("Element found at mid");
+System.out.println("Element found at mid index:"+mid);
 return;
 }
-if(arr[mid]<x)
-high=mid+1;
-{
-System.out.println("Element found at index:"+i);
-return;
 else
+{
 high=mid-1;
 }
+mid=(low+high)/2;
+}
+if(low>high)
+{
 System.out.println("Element not found");
 return;
-}
 }
 }
 }
