9d6a757b6e5e2c1b331e25190fa471547e8668f9
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-13 21:56:56 +0530

    Day6 programs done

---
+++ Day6/ForLoopPattern12.java
@@ -0,0 +1,59 @@
+
+/*
+      *
+     * *
+    *   *
+   *     *
+  *       *
+   *     *
+    *   *
+     * *
+      *
+
+*/
+import java.util.*;  
+public class ForLoopPattern12 
+{  
+public static void main(String[] args)  
+{  
+Scanner sc = new Scanner(System.in);  
+System.out.println("Enter the number : ");  
+int n = sc.nextInt();      
+for(int i=1;i<=n;i++)   
+{  
+for(int j=n;j>i;j--)   
+{  
+System.out.print(" ");  
+}  
+System.out.print("*");  
+for(int k=1;k<2*(i-1);k++)   
+{   
+System.out.print(" ");   
+}  
+if(i==1)   
+{   
+System.out.println("");  
+}  
+else  
+{  
+System.out.println("*");   
+}  
+}   
+for(int i=n-1;i>=1;i--)  
+{  
+for(int j=n;j>i;j--)   
+{  
+System.out.print(" ");  
+}  
+System.out.print("*");  
+for(int k=1;k<2*(i-1);k++)   
+{  
+System.out.print(" ");  
+}  
+if(i==1)  
+System.out.println("");  
+else  
+System.out.println("*");  
+}  
+}  
+}
