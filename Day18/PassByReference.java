12e48d6d7e3b8af5c3894f24e5b8540e531ddf80
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-16 21:55:38 +0530

    Day18 programs done

--- Day18/PassByReference.java
+++ Day18/PassByReference.java
@@ -3,16 +3,14 @@ public class PassByReference
 {
 public static void main(String args[])
 {
-int[] arr=new int[2];
-arr[0]=3;
-arr[1]=5;
+int arr[]={3,4,5};
 add(arr);
 System.out.println(Arrays.toString(arr));
 }
-static void add(int[] arr)
+static void add(int arr[])
 {
 for(int i=0;i<arr.length;i++)
-arr[0]=10;
+arr[1]=10;
 System.out.println(Arrays.toString(arr));
 }
 }
