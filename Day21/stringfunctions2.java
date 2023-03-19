87916ce1b3898507112f5f65346062750d404450
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-19 22:51:39 +0530

    Day21 programs done

--- Day21/stringfunctions2.java
+++ Day21/stringfunctions2.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class stringfunctions2
 {
 public static void main(String args[])
@@ -6,6 +7,6 @@ String s1="hello";
 String s2="hello";
 boolean res=s1.equals(s2);
 System.out.println(res);
-System.out.println(s2.getBytes());
+System.out.println(s2.contentEquals(s1));
 }
 }
