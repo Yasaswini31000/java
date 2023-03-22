8ab973116a19b467d446dd8680d55c297fbf55f7
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-22 18:36:07 +0530

    Day23 programs done

--- Day23/BorderArraysSum.java
+++ Day23/BorderArraysSum.java
@@ -13,19 +13,24 @@ for(int i=0;i<arr.length;i++)
 {
 for(int j=0;j<arr[i].length;j++)
 {
-if(i==0||j==arr[0].length-1)
+if(i==0||j==arr[i].length-1)
+{
 System.out.print(arr[i][j]+" ");
+sum+=arr[i][j];
 }
 }
-for(int i=0;i<arr.length;i++)
+}
+for(int i=arr.length-1;i>0;i--)
 {
-for(int j=arr[0].length-1;j>=0;j--)
+for(int j=arr[i].length-2;j>=0;j--)
 {
-if(i==arr.length-1&&j!=arr[0].length-1)
+if(j==0||i==arr[i].length-1)
+{
 System.out.print(arr[i][j]+" ");
 sum+=sum+arr[i][j];
 }
-System.out.print(sum+" ");
 }
+}
+System.out.println("\n"+sum);
 }
 }
