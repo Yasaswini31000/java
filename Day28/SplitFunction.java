4482ca224cce65bfc72824c432f37729e53bdba8
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-03-26 20:49:48 +0530

    Day28 programs done

---
+++ Day28/SplitFunction.java
@@ -0,0 +1,13 @@
+class SplitFunction
+{
+public static void main(String args[])
+{
+String str="welcome to java";
+String[] res=str.split(" ");
+System.out.print("result: ");
+for(String str2:res)
+{
+System.out.print(str2+" , ");
+}
+}
+}
---
+++ Day28/StringOperations.java
@@ -0,0 +1,9 @@
+public class StringOperations
+{
+public static void main(String args[])
+{
+String str="hello world";
+int len=str.length();
+System.out.println("length of string:"+len);
+}
+}
