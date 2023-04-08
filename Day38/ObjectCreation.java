25b35dd58d1738ee19ffe024bf10d1b98a6a78c7
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-04-08 13:51:10 +0530

    Day38 programs done

---
+++ Day38/ObjectCreation.java
@@ -0,0 +1,11 @@
+public class ObjectCreation
+{
+int x=5,y=6;
+public static void main(String args[])
+{
+ObjectCreation obj=new ObjectCreation();
+System.out.println(obj.x);
+System.out.println(obj.y);
+}
+}
+
