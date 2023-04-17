3a82874670799f01cb6165e8b5e104320c997d6a
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-04-17 17:33:54 +0530

    Day47 programs done

--- Day47/Sample.java
+++ Day47/Sample.java
@@ -1,8 +1,16 @@
+import java.util.*;
 public class Sample
 {
 public static void main(String args[])
 {
-System.out.println("Hello world");
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter any number :");
+int x=sc.nextInt();
+System.out.println("Enter any number :");
+int y=sc.nextInt(); 
+System.out.println("Enter any String :");
+String z=sc.next();
+System.out.println(x+y+" "+z);
 }
 }
 
