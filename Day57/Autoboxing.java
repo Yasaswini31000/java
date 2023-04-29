31347190367ac5fb8736bbefbc1f563c0cc0b1b1
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-04-29 21:31:28 +0530

    Day57 programs done

---
+++ Day57/Autoboxing.java
@@ -0,0 +1,25 @@
+class Autoboxing {
+
+    public static void main(String args[]) {
+    
+        int n1 = 120; 
+
+        Integer n2 = n1; 
+
+        System.out.println(n2);
+        
+        byte b1 = 120;
+        
+        Byte b2 = b1;
+        
+        System.out.println(b2);
+        
+        float f1 = 30.21f;
+        
+        Float f2 = f1;
+        
+        System.out.println(f2);
+        
+        
+    }
+}
---
+++ Day57/Unboxing.java
@@ -0,0 +1,23 @@
+public class Unboxing {
+    public static void main(String args[]) {
+        
+        Integer i1 = 120; 
+        int i2 = i1; 
+        
+        System.out.println(i2);
+        Boolean b1 = false;
+        boolean b2 = b1;
+        System.out.println(b2);
+        Float f1 = 31.00f;
+        float f2 = f1;
+        
+        System.out.println(f2);
+        Byte a1 = 122;
+        byte a2 = a1;
+
+        System.out.println(a2);
+
+
+
+    }
+}
