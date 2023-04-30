47e1bf3c16eac1ab55b4dfd81a0e867378f238c4
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-04-30 22:13:55 +0530

    Day59 programs done

---
+++ Day59/BigInteger1.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+
+public class BigInteger1 {
+
+    public static void main(String args[]) {
+    
+        BigInteger b1 = new BigInteger("32451");
+        
+        BigInteger b2 = new BigInteger("14637");
+        
+        System.out.println(b1.add(b2));
+        
+        System.out.println(b1.subtract(b2));
+        
+        System.out.println(b1.multiply(b2));
+        
+        System.out.println(b1.divide(b2));
+        
+    }
+    
+}
+    
+
