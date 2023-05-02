8932801d6f0d2e855d8dcc7787d1ef1f60f60fea
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-05-02 20:43:04 +0530

    Day61 programs done

---
+++ Day61/ArithmeticException.java
@@ -0,0 +1,13 @@
+public class ArithmeticException {
+
+     public static void main(String args[]) {
+
+         int x=10;
+         
+         int y=x/0;
+         
+         System.out.println(y);
+     
+     }
+     
+}
