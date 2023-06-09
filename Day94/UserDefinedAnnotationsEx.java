8a1c8ff5bc53e0887a1e5e429e86198246ef3f1d
Yasaswini31000 <yasaswiniagadde@gmail.com>
2023-06-09 12:19:34 +0530

    Day94 programs done

---
+++ Day94/UserDefinedAnnotationsEx.java
@@ -0,0 +1,16 @@
+public class UserDefinedAnnotationsEx {
+    @isAProgrammer
+    String str = "Hello java";
+
+        @isAProgrammer
+        public static void main(@isAProgrammer String[] args) {
+
+        }
+        @isAProgrammer
+        UserDefinedAnnotationsEx() {
+
+        }
+    }
+    @interface isAProgrammer {
+
+    }
